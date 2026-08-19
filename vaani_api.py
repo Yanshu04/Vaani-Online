cba3168f4aa782d661ca763150fee3579bf82abc
Yanshu04 <yanshushingala@gmail.com>
2026-08-19 13:34:06 +0530

    Add fastapi, uvicorn to requirements.txt and support dynamic PORT binding

--- vaani_api.py
+++ vaani_api.py
@@ -1,5 +1,8 @@
+import os
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("app.api:app", host="0.0.0.0", port=8001, reload=False)
+    port = int(os.getenv("PORT", 8001))
+    uvicorn.run("app.api:app", host="0.0.0.0", port=port, reload=False)
+
 
